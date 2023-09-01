69d51a703a84a452594e1689acdb21ff28e17f1d
Aarush-Shukla <98807835+Aarush-Shukla@users.noreply.github.com>
2023-09-01 07:38:42 +0530

    Update Leftmost_And_Rightmost_Nodes_Of_Binary_Tree.cpp

--- Leftmost_And_Rightmost_Nodes_Of_Binary_Tree.cpp
+++ Leftmost_And_Rightmost_Nodes_Of_Binary_Tree.cpp
@@ -1,1 +1,45 @@
+void printCorner(Node *root)
+{
 
+ queue<Node*>q;
+    q.push(root);
+    
+    vector<int>ans;
+    while(!q.empty())
+    {
+        
+        int size=q.size();
+     
+            int count=1;
+            
+            while(count<=size)
+            {
+                Node *temp=q.front();
+                q.pop();
+                if(count==1 || count==size)
+                {
+                    ans.push_back(temp->data);
+                    
+                }
+                
+                if(temp->left!= NULL)
+                    {
+                        q.push(temp->left);
+                    }
+                    
+                    if(temp->right!=NULL)
+                    {
+                        q.push(temp->right);
+                    }
+                    count++;
+                
+            }
+            
+        }
+        
+        for(auto it: ans)
+        {
+            cout<<it<<" ";
+        }
+
+}
