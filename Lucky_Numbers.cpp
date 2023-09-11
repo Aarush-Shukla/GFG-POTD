4fff9e354ee5f4646942fd7c102d27a94b1eb862
Aarush-Shukla <98807835+Aarush-Shukla@users.noreply.github.com>
2023-09-11 10:47:39 +0530

    Create Lucky_Numbers.cpp

---
+++ Lucky_Numbers.cpp
@@ -0,0 +1,11 @@
+for(int i=2;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+            
+            n=n-(n/i);
+        }
+        
+        return true;
