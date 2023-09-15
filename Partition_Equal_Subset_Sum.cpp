3cafdce80aafe657af23b385d4bf92fd0b10d77f
Aarush-Shukla <98807835+Aarush-Shukla@users.noreply.github.com>
2023-09-15 06:56:19 +0530

    Create Partition_Equal_Subset_Sum.cpp

---
+++ Partition_Equal_Subset_Sum.cpp
@@ -0,0 +1,51 @@
+bool isSubsetSum(int arr[], int n, int sum,
+                 vector<vector<int> >& dp)
+{
+    // Base Cases
+    if (sum == 0)
+        return true;
+    if (n == 0 && sum != 0)
+        return false;
+ 
+    // return solved subproblem
+    if (dp[n][sum] != -1) {
+        return dp[n][sum];
+    }
+ 
+    // If last element is greater than sum, then
+    // ignore it
+    if (arr[n - 1] > sum)
+        return dp[n][sum]=isSubsetSum(arr, n - 1, sum, dp);
+ 
+    /* else, check if sum can be obtained by any of
+        the following
+        (a) including the last element
+        (b) excluding the last element
+    */
+    // also store the subproblem in dp matrix
+    return dp[n][sum]
+           = isSubsetSum(arr, n - 1, sum, dp)
+             || isSubsetSum(arr, n - 1, sum - arr[n - 1],
+                            dp);
+}
+
+    int equalPartition(int N, int arr[])
+    {
+         // Calculate sum of the elements in array
+    int sum = 0;
+    for (int i = 0; i < N; i++)
+        sum += arr[i];
+ 
+    // If sum is odd, there cannot be two subsets
+    // with equal sum
+    if (sum % 2 != 0)
+        return false;
+ 
+    // To store overlapping subproblems
+    vector<vector<int> > dp(N + 1,
+                            vector<int>(sum + 1, -1));
+ 
+    // Find if there is subset with sum equal to
+    // half of total sum
+    return isSubsetSum(arr, N, sum / 2, dp);
+    }
